5a3291b29c9a210f117f8315032ac6112f074831
MarselMi <marselmihajlov@yandex.ru>
2023-01-23 19:36:20 +0300

    base_changes

--- spaffaccaunting/prod_settings.py
+++ spaffaccaunting/prod_settings.py
@@ -37,6 +37,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
         },
         'NAME': 'buh',
         'USER': 'buhsoft',
