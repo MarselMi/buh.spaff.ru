a62651ffe9ec8a5e894205dee31e49fae09e3f3d
MarselMi <marselmihajlov@yandex.ru>
2023-10-14 12:40:21 +0500

    tr_import

--- mainapp/tasks.py
+++ mainapp/tasks.py
@@ -31,7 +31,7 @@ def import_transactions():
                 for transaction in transactions_objects:
                     transaction_new = Transaction
                     sub_type = None
-                    if len(Transaction.objects.filter(import_id=transaction.get("operationId"))) == 0:
+                    if Transaction.objects.filter(import_id=transaction.get("operationId")).exists() is False:
                         pay_type = PayType.objects.filter(pay_type='Временная категория')
                         if transaction.get('paymentPurpose').lower().find('cloudpayments') > -1:
                             pay_type = PayType.objects.filter(pay_type='CloudPayments')
